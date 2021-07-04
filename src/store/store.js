c15e2d1b3ec62f36a8bd87f78a5e0cc4ac058c45
Peterson <petersonsimiao1@gmail.com>
2021-07-04 17:06:13 -0300

    feat: ligando inputs ao store do vuex

--- src/store/store.js
+++ src/store/store.js
@@ -4,18 +4,29 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-        produtos: []
+  state: {
+    produtos: [],
+    quantidade: 0,
+    preco:0
+  },
+  getters:{
+    valorTotal(state) {
+      return state.produtos.map(({quantidade, preco}) => quantidade * preco)
+      .reduce((total, atual) => total + atual, 0)
     },
-    getters:{
-        valorTotal(state) {
-            return state.produtos.map(p => p.quantidade * p.preco)
-            .reduce((total, atual) => total + atual, 0)
-        }
+  },
+  mutations: {
+    adicionarProduto(state, produto){
+      state.produtos.push(produto);
     },
-    mutations: {
-        adicionarProduto(state, payload){
-            state.produtos.push(payload);
-        }
+    setQuantidade(state, quantidade){
+      state.quantidade = quantidade;
+    },
+    setPreco(state, preco){
+      state.preco = preco;
     }
+  },
+  actions: {
+      
+  }
 })
